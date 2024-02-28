6db5e9bebf3f4194cdcac17348f369ca0da08782
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-02-28 21:23:06 +0300

    вынес всё в корневую папку

---
+++ src/constants/images.ts
@@ -0,0 +1,17 @@
+import partyIMG1 from "../assets/images/backItem.jpg";
+import partyIMG2 from "../assets/images/backItem2.png";
+import partyIMG3 from "../assets/images/backItem3.png";
+import partyIMG4 from "../assets/images/backItem4.png";
+import partyIMG5 from "../assets/images/backItem5.png";
+import partyIMG6 from "../assets/images/backItem6.png";
+import partyIMG7 from "../assets/images/backItem7.png";
+import partyIMG8 from "../assets/images/backItem8.png";
+
+export const partyImage1 = partyIMG1;
+export const partyImage2 = partyIMG2;
+export const partyImage3 = partyIMG3;
+export const partyImage4 = partyIMG4;
+export const partyImage5 = partyIMG5;
+export const partyImage6 = partyIMG6;
+export const partyImage7 = partyIMG7;
+export const partyImage8 = partyIMG8;
